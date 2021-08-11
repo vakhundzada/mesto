c3831889226e846b9c7598f5dd78f330eaf93635
vakhundzada <akhundzadevaqif@gmail.com>
2021-08-11 14:40:30 +0400

    Refactor: additional changes

--- script.js
+++ script.js
@@ -4,10 +4,7 @@ const initialCards = [
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
   },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-  },
+
   {
     name: 'Иваново',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
